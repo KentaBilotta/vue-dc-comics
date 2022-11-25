249297b4614304c550014529e798bbb296a5ae18
Kenta Bilotta <kentabilotta20@gmail.com>
2022-11-25 20:52:16 +0100

    arrSeries + CardPage

---
+++ src/components/CardPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <img :src="imgUrl" alt="">
+    <div class="title">{{ titolo }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPage',
+  props: {
+    imgUrl: String,
+    titolo: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card{
+  color: white;
+  text-transform: uppercase;
+  width: calc(100% / 8);
+  margin: 15px;
+
+  img{
+    width: 100%;
+    height: 200px;;
+    object-fit: cover;
+    object-position: top center;
+  }
+
+  .title{
+    font-weight: lighter;
+    margin-top: 20px;
+  }
+}
+
+</style>
